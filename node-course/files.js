95e330ba3088ea6f8f36458c5ec3e465415f13df
Chaker Amine <chkramine2003@gmail.com>
2023-02-24 09:22:16 +0100

    'node-basics'

---
+++ node-course/files.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+// reading files
+/* the second function fires after the reading is done and it takes two args err and data and it is async so it takes some time to complete*/
+fs.readFile("./docs/blog.txt", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log(data.toString());
+});
+
+// writing files
+fs.writeFile("./docs/written.txt", "hello , world", () => {
+  console.log("file was written");
+});
+
+//deleting files
+if (fs.existsSync("./docs/delteme.txt")) {
+  fs.unlink("./docs/delteme.txt", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("file deleted");
+  });
+}
